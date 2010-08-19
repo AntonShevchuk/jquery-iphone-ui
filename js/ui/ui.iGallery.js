82c678147f16f7fb9678486ba665d544655f0d13
AntonShevchuk <AntonShevchuk@2479db0e-9323-11de-b51d-eb253e4b719b>
2010-08-19 11:08:57 +0000

    Update JQuery and UI Fixed Issue #10 :)

--- js/ui/ui.iGallery.js
+++ js/ui/ui.iGallery.js
@@ -1,11 +1,11 @@
 /**
- * Create image gallery
+ * Create image/content gallery
  *
  * @author Anton Shevchuk (http://anton.shevchuk.name)
  * @copyright (c) 2009 jQuery iPhone UI (http://iphone.hohli.com)
  * @license   Dual licensed under the MIT (MIT-LICENSE.txt) and GPL (GPL-LICENSE.txt) licenses.
  * 
- * @version 0.2
+ * @version 0.3
  */
 (function($){
     $.widget('ui.iGallery', {
@@ -18,12 +18,17 @@
 			
 			// FIXME: need refactoring - wrapInner - it's bad
             $this.wrapInner('<div class="frame"></div>');
-
-            this.frame = $this.find('.frame');
- 
+            if ($('#icursorw').length == 0)
+                $('body').append('<div id="icursorw"></div>');
             
+            this.frame = $this.find('.frame');
+            this.cursor = $('#icursorw');
+            this.elementMargin = $this.position().left;
+            
+            // 20px - it's margin between slides
             var length = 0;
-            var width  = $this.width();
+            var space  = this.space = 20;
+            var width  = this.width = $this.width();
             
             // filter
             this.frame.children('br,hr').remove();
@@ -38,15 +43,15 @@
             this.oldX = 0;
             this.newX = 0;
             this.move = false;
+            this.click = false;
             
-            // 20px - it's margin between slides
             this.maxMargin = 0;
-            this.minMargin = - ((width+20)*(length - 1)+40);
-            this.marginLeft = -20;
+            this.minMargin = - ((width+space)*(length - 1)+space*2);
+            this.marginLeft = -space;
             
             // set container width
             this.frame.css({
-                width:      (width+20)*length+20,
+                width:      (width+space)*length+space,
                 marginLeft: this.marginLeft
             });
             
@@ -56,35 +61,115 @@
     		    _self.oldX = event.pageX;
                 _self.move = true;
                 
+                // check it's click or d'n'd
+                _self.click = true;
+                setTimeout(function(){
+                    _self.click = false;
+                }, 200);
+                
     		    event.stopPropagation();
     		    event.preventDefault();
             });
             
-            $(document).mouseup(function(event){
-                if (_self.move) {
-                    _self.move = false;
-                    var el = Math.round(_self.marginLeft/(width+20));
-                    _self.marginLeft = el * (width+20) - 20;
-                    _self._scroll();
+			// click on image
+            this.element.click(function(event) {
+                if (_self.click) {
+                    
+        		    var x = event.pageX - (width + _self.elementMargin);
+        		    console.log(event.pageX, event.clientX, document.body.scrollLeft);
+        		    if (0 < x && x < (width/2)) {
+                        _self._prev();
+        		    } else {
+                        _self._next();
+        		    }
+                    
+        		    event.stopPropagation();
+        		    event.preventDefault();
                 }
             });
             
+            // mousewheel
+            if (typeof $.fn.mousewheel == 'function') {
+			    // scroll by mousewheel
+			    this.element.mousewheel(function (event, direction) {
+			         if ( direction < 0 ) {
+			             _self._next();
+			         } else {
+			             _self._prev();
+			         }
+        		     event.stopPropagation();
+        		     event.preventDefault();
+			    });
+			}
+            
+			// custom cursor
+            this.element.mouseover(function(event) {
+                _self.cursor.show();
+                return true;
+            });
+            this.element.mousemove(function(event) {
+                _self.cursor.css({
+                    top:event.pageY,
+                    left:event.pageX,
+                });
+                return true;
+            });
+            this.element.mouseout(function(event) {
+                _self.cursor.hide();
+                return true;
+            });
+			
+            // document events
+            $(document).mouseup(function(event){
+                if (_self.move) {
+                    _self.move = false;
+                    var el = Math.round(_self.marginLeft/(width+space));
+                    _self.marginLeft = el * (width+space) - space;
+                    _self._scroll();
+                }
+                return true;
+            });
             $(document).mousemove(function(event){
                 if (_self.move && _self.oldX != 0) {
                     _self.newX = event.pageX;
                     
                     var diff = _self.marginLeft + (_self.newX - _self.oldX);                    
                     if (diff >= _self.minMargin && diff <= _self.maxMargin) {
-                        _self.marginLeft =  _self.marginLeft + (_self.newX - _self.oldX);
+                        _self.marginLeft = _self.marginLeft + (_self.newX - _self.oldX);
                         _self._scroll();
                     }
                     _self.oldX = _self.newX;
                 }
+                if ($.browser.msie) {
+                    document.Show.MouseX.value = event.pageX;
+                    document.Show.MouseY.value = event.pageY;
+                }
+                return true;
             });
         },
         _scroll: function() {
             this.frame.stop(true,true);
             this.frame.animate({'marginLeft':this.marginLeft});
+        },
+        _next: function() {
+            if (this.minMargin < this.marginLeft - (this.width+this.space)) {
+                var el = Math.round(this.marginLeft/(this.width+this.space));
+                this.marginLeft = (el-1) * (this.width+this.space) - this.space;
+                this._scroll();
+            } else {
+                this.frame.animate({'marginLeft':this.marginLeft-20})
+                          .animate({'marginLeft':this.marginLeft});
+            }
+        },
+        _prev: function() {
+            if (this.maxMargin > this.marginLeft + (this.width+this.space)) {
+                var el = Math.round(this.marginLeft/(this.width+this.space));
+                this.marginLeft = (el+1) * (this.width+this.space) - this.space;
+                this._scroll();
+            } else {
+                this.frame.animate({'marginLeft':this.marginLeft+20})
+                          .animate({'marginLeft':this.marginLeft});
+            }
         }
         
     });    
--- js/ui/ui.iScroll.js
+++ js/ui/ui.iScroll.js
@@ -5,25 +5,25 @@
  * @copyright (c) 2009 jQuery iPhone UI (http://iphone.hohli.com)
  * @license   Dual licensed under the MIT (MIT-LICENSE.txt) and GPL (GPL-LICENSE.txt) licenses.
  * 
- * @version 0.1
+ * @version 0.2
  */
 (function($){
     $.widget('ui.iScroll', {
-		 options:{
-		 	height:null
-		 },
+         options:{
+             height:null
+         },
          _init: function(){
              var _self = this;
              
-			 if (!this.options.height) {
-			 	this.height = this.element.outerHeight();
-			 } else {
-			 	this.height = this.options.height;
-			 }
-        	 
-			 
+             if (!this.options.height) {
+                 this.height = this.element.outerHeight();
+             } else {
+                 this.height = this.options.height;
+             }
+             
+             
              // build in dom
-			 this.element.addClass('iphoneui');
+             this.element.addClass('iphoneui');
              this.element.wrap($('<div />').addClass('iscrollui')); 
              this.element.after('<div class="iscrollbar"><div></div></div>');
              
@@ -31,52 +31,54 @@
 
              this.container.css({height: this.height, overflow:'hidden'});
              this.element.css({height:'auto'});
-			 
+             
              // "global" variables
-    		 this.difference = this.element.height() - this.height;
-    		 if (this.difference <= 0) return;
-    		 
-    		 this.ratio = this.height/this.element.height();
-    		 this.marginTop = 0;
+             this.difference = this.element.height() - this.height;
+             if (this.difference <= 0) return;
+             
+             this.ratio = this.height/this.element.height();
+             this.marginTop = 0;
              this.oldY = 0;
              this.newY = 0;
+             this.direction = 0;
              this.move = false;
              this.TO = null;
+             this.timer = 0;
+             this.timerTO = null;
              
-        	 // scrollbar
-        	 this.scrollbar  = this.container.find('.iscrollbar');
-        	 this.scroller   = this.container.find('.iscrollbar div');
+             // scrollbar
+             this.scrollbar  = this.container.find('.iscrollbar');
+             this.scroller   = this.container.find('.iscrollbar div');
              
              // change height of scrollbar  
-        	 this.scrollbar.height(this.height);
-    		 this.scroller.height(this.ratio*this.container.height());
-    		 
-
+             this.scrollbar.height(this.height);
+             this.scroller.height(this.ratio*this.container.height());
+             
              // http://www.jdempster.com/category/jquery/disabletextselect/
              if (typeof $.fn.disableTextSelect == 'function') {
-                this.container.disableTextSelect();
+                 this.container.disableTextSelect();
              }
              
-			 if (typeof $.fn.mousewheel != 'function') {
-			 	 console.error('Mousewheel jQuery plugin is required for iPhone.iScroll widget. More information on http://iphone.hohli.com');
-				 return;
-			 }
-			 
-             this.container.mousewheel( function (event, direction)  {                
+             if (typeof $.fn.mousewheel != 'function') {
+                 console.error('Mousewheel jQuery plugin is required for iPhone.iScroll widget. More information on http://iphone.hohli.com');
+                 return;
+             }
+             
+             this.container.mousewheel( function (event, direction) {                
                 if ( _self.marginTop <= 0  && ( _self.marginTop + _self.difference >= 0 ) ) {
-        		        _self.element.stop();
-        		            			
-        			    if ( direction < 0 ){
-        			        _self.marginTop -= 60;
-        			        _self.element.animate({'marginTop':_self.marginTop}, 
+                        _self.element.stop();
+                                        
+                        if ( direction < 0 ) {
+                            _self.marginTop -= 60;
+                            _self.element.animate({'marginTop':_self.marginTop}, 
                                 function(){
                                     if( _self.difference + _self.marginTop < 0 ) {
                                         _self.marginTop = -_self.difference;
                                         _self.element.animate({'marginTop':_self.marginTop});
                                     }
                                 });
-        			    } else {
-        				    _self.marginTop += 60;
+                        } else {
+                            _self.marginTop += 60;
                             _self.element
                                 .animate({'marginTop':_self.marginTop}, 
                                     function(){
@@ -85,36 +87,70 @@
                                             _self.element.animate({'marginTop':_self.marginTop});
                                         }
                                     });
-        			    }
-        			    _self._scroll();
-        			}
-        		    event.stopPropagation();
-        		    event.preventDefault();
-    		 });
-    		
-    		this.container.mousedown(function(event) {
-    		    _self.element.stop();
-    		    _self.oldY = event.pageY;
+                        }
+                        _self._scroll();
+                    }
+                    event.stopPropagation();
+                    event.preventDefault();
+             });
+            
+            this.container.mousedown(function(event) {
+                _self.element.stop();
+                _self.oldY = event.pageY;
                 _self.move = true;
+                
+//                _self.timerTO = setInterval(function(){
+//                    _self.timer++;
+//                }, 100);
+                
                 /*_self._showScroll();*/
             });
             
             $(document).mouseup(function(event){
+                if (!_self.move) return;
+                
+//                clearInterval(_self.timerTO);                
+//                var Y = _self.oldY;
+//                var T = _self.timer;
+//                console.log(Y,T, _self.newY);
+                
                 _self.move = false;
                 _self.oldY = 0;
+//                _self.timer = 0;
+                
                 if (_self.marginTop > 0) {
+                    // empty top zone
                     _self.marginTop = 0;
                     _self.element.animate({'marginTop':_self.marginTop});
-                } else if( _self.difference + _self.marginTop < 0 ) {
+                } else if (_self.difference + _self.marginTop < 0 ) {
+                    // empty bottom zone
                     _self.marginTop = -_self.difference;
+                    _self.element.animate({'marginTop':_self.marginTop});
+                } else if (_self.direction < 0) {
+                    // after mouseup scroll to up
+                    if (_self.difference + _self.marginTop < 24) {
+                        _self.marginTop = -_self.difference;
+                    } else {
+                        _self.marginTop = _self.marginTop + 24;
+                    }                    
+                    _self.element.animate({'marginTop':_self.marginTop});
+                } else if (_self.direction > 0) {
+                    // after mouseup scroll to down
+                    if ( _self.marginTop > -24) {
+                        _self.marginTop = 0;
+                    } else {
+                        _self.marginTop = _self.marginTop - 24;
+                    }
                     _self.element.animate({'marginTop':_self.marginTop});
                 }
             });
+            
             $(document).mousemove(function(event){
-                if (_self.move && _self.oldY!=0) {
+                if (_self.move && _self.oldY != 0) {
                     _self.newY = event.pageY;
                     _self.marginTop = _self.marginTop + _self.newY - _self.oldY;
                     _self.element.css({ 'marginTop' : _self.marginTop});
+                    _self.direction = (_self.newY > _self.oldY) ? -1 : 1;
                     _self.oldY = _self.newY;
                     
                     _self._scroll();
@@ -124,13 +160,13 @@
         _scroll:function() {
              this._showScroll();
              this.scroller.stop(true,true);
-		     if ( this.marginTop <= 0  && ( this.marginTop + this.difference >= 0 ) ) {
-		         this.scroller.animate({'marginTop':Math.abs(this.ratio*this.marginTop)},100);
-		     } else if (this.marginTop > 0) {
-		         this.scroller.animate({'marginTop':0},100);
-		     } else if ( this.marginTop + this.difference <= 0  ) {
-		         this.scroller.animate({'marginTop':Math.abs(this.ratio*this.difference)},100);
-		     }
+             if ( this.marginTop <= 0  && ( this.marginTop + this.difference >= 0 ) ) {
+                 this.scroller.animate({'marginTop':Math.abs(this.ratio*this.marginTop)},100);
+             } else if (this.marginTop > 0) {
+                 this.scroller.animate({'marginTop':0},100);
+             } else if ( this.marginTop + this.difference <= 0  ) {
+                 this.scroller.animate({'marginTop':Math.abs(this.ratio*this.difference)},100);
+             }
         },
         _showScroll:function() {
             var _self = this;
